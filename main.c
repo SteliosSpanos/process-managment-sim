0469e0286849d823a2da2ea89b26a4067fe0f159
SteliosSpanos <stylspan19@gmail.com>
2025-11-29 21:13:08 +0200

    Tries new features

--- main.c
+++ main.c
@@ -3,25 +3,22 @@
 #include <stdio.h>
 #include "process.h"
 #include "ready-queue.h"
+#include "scheduler.h"
 
 
 int main() {
-	queue q;
-  	init_queue(&q);
+	process processes[3];
 
-  	process p1, p2, p3;
-  	init_process(&p1, 12, 0, 8, 2);   // PID=12, arrival=0, burst=8, priority=2
-  	init_process(&p2, 18, 1, 4, 1);   // PID=18, arrival=1, burst=4, priority=1
-  	init_process(&p3, 34, 2, 9, 3);   // PID=34, arrival=2, burst=9, priority=3
+  	init_process(&processes[0], 1, 0, 8, 2);
+  	init_process(&processes[1], 2, 1, 4, 1);
+  	init_process(&processes[2], 3, 2, 9, 3);
 
-  	enqueue(&q, p1);
-  	enqueue(&q, p2);
-  	enqueue(&q, p3);
+  	// Run FCFS scheduler
+  	scheduler_stats stats;
+  	fcfs(processes, 3, &stats);
 
-  	display_queue(&q);
 
-  	free_queue(&q);
   	return 0;
-	
+		
 }
 	
