58b11786a95068c95336e3ca3422808e9fd05f0f
SteliosSpanos <stylspan19@gmail.com>
2025-11-29 18:18:58 +0000

    Adjusts the init_process function to the new attributes

--- process.c
+++ process.c
@@ -1,6 +1,10 @@
 #include "process.h"
 
-void init_process(process *p, unsigned int pid) {
+void init_process(process *p, unsigned int pid, unsigned int arrival_time, unsigned int burst_time, unsigned int priority) {
+	p->state = NEW;
 	p->pid = pid;
+	p->arrival_time = arrival_time;
+	p->burst_time = burst_time;
+	p->priority = priority;
 }
 
