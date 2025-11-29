841d79439af66f9ad16aa836ab24b3835426b9e7
SteliosSpanos <stylspan19@gmail.com>
2025-11-29 20:26:05 +0200

    Adjust the init_process function to the new attributes

--- process.c
+++ process.c
@@ -6,5 +6,10 @@ void init_process(process *p, unsigned int pid, unsigned int arrival_time, unsig
 	p->arrival_time = arrival_time;
 	p->burst_time = burst_time;
 	p->priority = priority;
-}
+  p->remaining_time = burst_time;
+  p->waiting_time = 0;
+  p->turnaround_time = 0;
+  p->response_time = 0;
+  p->first_run = 0;
+
 
