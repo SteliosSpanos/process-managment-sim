9adb08e0cad732752a8807276801671c39116bad
SteliosSpanos <stylspan19@gmail.com>
2025-11-29 20:05:28 +0200

    Adds more attributes to process struct

--- process.h
+++ process.h
@@ -2,10 +2,27 @@
 #define TRUE 1
 #define FALSE 0
 
+typedef enum {
+	NEW,
+	READY,
+	WAITING,
+	TERMINATED
+} peocess_state;
+
+
 typedef struct process {
 	unsigned int pid;
+	process_state state;
+	unsigned int arrival_time;
+	unsigned int remaining_time;
+	unsigned int burst_time;
+	unsigned int priority;
+	unsigned int waiting_time;
+	unsigned int turnaround_time;
+	unsigned int response_time;
+	int first_run;
 } process;
 
-void init_process(process *p, unsigned int pid);
+void init_process(process *p, unsigned int pid, unsigned int arrival_time, unsigned int burst_time, unsigned int priority);
 
 
